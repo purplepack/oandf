fb2fe25583c64e37f2e39a747e10de79baf894ac
Rexedge <ap.oyeniran@gmail.com>
2023-10-29 12:43:48 +0100

    modified dashboard

--- app/(root)/dashboard/page.tsx
+++ app/(root)/dashboard/page.tsx
@@ -5,8 +5,9 @@ import React from 'react';
 export default async function DashboardPage() {
 	const company = await getCompanyDetails();
 	return (
-		<div className='grid h-screen place-items-center'>
-			<Card className='p-5 bg-primary text-background'>
+		<div className='flex flex-col h-screen items-center pt-10 gap-5'>
+			<h1 className='text-3xl font-medium'>DASHBOARD</h1>
+			<Card className='p-5 bg-primary text-background max-w-lg'>
 				<div className=''>Name: {company?.name}</div>
 				<div className=''>Description: {company?.description}</div>
 				<div className=''>Industry: {company?.industry}</div>
